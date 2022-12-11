087c6ac7e933b6106eba56861745eef82fbc5b25
Viren Khatri <44755140+werainkhatri@users.noreply.github.com>
2022-12-11 12:36:19 +0530

    add keyboard arrow key control

--- lib/slide_scaffold.dart
+++ lib/slide_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ppp/widgets/nav_button.dart';
 import 'package:ppp/utils/functions.dart';
@@ -20,6 +21,7 @@ class _SlideScaffoldState extends State<SlideScaffold> {
       return MaterialRectArcTween(begin: begin, end: end);
     },
   );
+  final FocusNode keyboardFocusNode = FocusNode();
 
   void _onNext() {
     if (_currentSlide < widget.slides.length - 1) {
@@ -45,46 +47,59 @@ class _SlideScaffoldState extends State<SlideScaffold> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: HeroControllerScope(
-        controller: _heroController,
-        child: Navigator(
-          key: _navigatorKey,
-          initialRoute: _currentSlide.toString(),
-          onGenerateRoute: (settings) => PageRouteBuilder(
-            pageBuilder: (context, animation1, animation2) => DefaultTextStyle(
-              style: GoogleFonts.rubik(fontWeight: FontWeight.w600),
-              child: F.getSlideFromName(settings.name, widget.slides),
+    // todo: also listen to ppt remote next/previous.
+    return KeyboardListener(
+      focusNode: keyboardFocusNode,
+      onKeyEvent: (KeyEvent event) {
+        if (event is KeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+            _onNext();
+          } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+            _onPrevious();
+          }
+        }
+      },
+      child: Scaffold(
+        body: HeroControllerScope(
+          controller: _heroController,
+          child: Navigator(
+            key: _navigatorKey,
+            initialRoute: _currentSlide.toString(),
+            onGenerateRoute: (settings) => PageRouteBuilder(
+              pageBuilder: (context, animation1, animation2) => DefaultTextStyle(
+                style: GoogleFonts.rubik(fontWeight: FontWeight.w600),
+                child: F.getSlideFromName(settings.name, widget.slides),
+              ),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const right = Offset(-1.0, 0.0);
+                const center = Offset.zero;
+                const left = Offset(1.0, 0.0);
+                final inTween = Tween(begin: left, end: center);
+                final outTween = Tween(begin: center, end: right);
+                final Animation<Offset> offsetAnimation;
+
+                if (animation.value == 1.0 && secondaryAnimation.value != 0.0) {
+                  offsetAnimation = secondaryAnimation.drive(outTween);
+                } else {
+                  offsetAnimation = animation.drive(inTween);
+                }
+
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: child,
+                );
+              },
             ),
-            transitionsBuilder: (context, animation, secondaryAnimation, child) {
-              const right = Offset(-1.0, 0.0);
-              const center = Offset.zero;
-              const left = Offset(1.0, 0.0);
-              final inTween = Tween(begin: left, end: center);
-              final outTween = Tween(begin: center, end: right);
-              final Animation<Offset> offsetAnimation;
-
-              if (animation.value == 1.0 && secondaryAnimation.value != 0.0) {
-                offsetAnimation = secondaryAnimation.drive(outTween);
-              } else {
-                offsetAnimation = animation.drive(inTween);
-              }
-
-              return SlideTransition(
-                position: offsetAnimation,
-                child: child,
-              );
-            },
           ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          NavButton(icon: Icons.arrow_left_rounded, onPressed: _onPrevious),
-          const SizedBox(width: 10),
-          NavButton(icon: Icons.arrow_right_rounded, onPressed: _onNext),
-        ],
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            NavButton(icon: Icons.arrow_left_rounded, onPressed: _onPrevious),
+            const SizedBox(width: 10),
+            NavButton(icon: Icons.arrow_right_rounded, onPressed: _onNext),
+          ],
+        ),
       ),
     );
   }
